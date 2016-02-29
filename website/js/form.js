729b8d7c38e05d3983556692c3baef793957abb7
Zhou Yongguo <zhouyongguo@wisdomgarden.com>
2016-02-29 21:04:06 +0800

    template

--- website/js/form.js
+++ website/js/form.js
@@ -1,23 +1,44 @@
 $(function () {
     var form = (function () {
-        var completedCount = 0;
-        var terms = [];
+        var $currentFieldItem;
         var $form = $("#form form");
+        var $fieldsDiv = $("#fieldsDiv");
+        var $fieldAttDiv = $("#fieldAttDiv");
+        var showFieldTab=function(){
+            $("#fieldsTab").addClass("active").siblings().removeClass("active");
+            $fieldsDiv.show();
+            $fieldAttDiv.hide();
+        };
+        var showFieldAttTab=function(){
+            $("#fieldAttTab").addClass("active").siblings().removeClass("active");
+            $fieldsDiv.hide();
+            $fieldAttDiv.show();
+
+        };
         var init = function () {
 
             $(".custom-fields .btn").on("click", function(){
 
-                $field=$("#htmlField").find("div[data-field='text']").clone();
+                var $field=$("#htmlField").find("div[data-field='text']").clone();
                 $form.append($field);
                 $field.find('[data-toggle="tooltip"]').tooltip();
             });
-            $("body").on("click",'a[data-toggle="tooltip"]',function(){
 
-                $(this).closest(".field-item").remove();
-
+            $("#fieldsTab").on('click',function(){
+                showFieldTab();
             });
-
-
+            $("#fieldAttTab").on('click',function(){
+                showFieldAttTab();
+            });
+            $("body").on("click",'.field-item',function(){
+               $(this).addClass("active").siblings().removeClass("active");
+               $currentFieldItem=$(this);
+               showFieldAttTab();
+            });
+            $("body").on("click",'a[data-toggle="tooltip"]',function(event){
+                event.stopPropagation();
+                $(this).closest(".field-item").remove();
+            });
         };
         return {
             init: init
